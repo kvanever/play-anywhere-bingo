1b1c821f2444125c49b5ff93df25d2918010bbe2
Bang Nguyen and Kevin VanEvery <kevin@>
2016-06-21 11:52:37 -0700

    add random words to player 1 board

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 var bingoArray = [];
 
-
 window.onload = function() {
 		var fileInput = document.getElementById('fileInput');
 		var fileDisplayArea = document.getElementById('fileDisplayArea');
@@ -23,5 +22,29 @@ window.onload = function() {
 			} else {
 				fileDisplayArea.innerText = "File not supported!"
 			}
+			Board.grid.forEach
+			$(".board").append("<div class='square'><div class='content' id='1'>" + Board.grid[i][j].string + "</div></div>")
 		});
 }
+
+var Square = function (string) {
+	this.string = string;
+	this.marked = false;
+}
+
+// Board constructor - 5 squares makes Bingo
+
+var Board = function (squares) {
+	this.squares = squares;
+	this.grid = [];
+}
+
+Board.prototype.buildBoard = function() {
+    var i,j;
+    for(i = 0; i < this.squares; i++){
+        this.grid[i] = [];
+        for(j = 0; j < this.squares; j++){
+            this.grid[i][j] = new Square(bingoArray[Math.floor(Math.random() * bingoArray.length)]);
+        }
+    }
+};
